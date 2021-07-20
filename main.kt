ce3c94a2df9599b55b0d7941c0d56119e83d9b95
chukky65 <85235490+chukky65@users.noreply.github.com>
2021-07-20 17:50:26 +0100

    reverse of array(int)

--- main.kt
+++ main.kt
@@ -1,22 +1,13 @@
-fun main(args: Array<String>) {
-    println("Advantages of Agile Sofware Development")
-    println("1) Working software is delivered frequently")
-    println("2) Face to Face conversation is the best form of communication")
-    println("3) Regular adaptation to changing circumstances")
-    println("4) Continous attention to technical excellence")
-    println("5) People and interactions are emphasized rather than process and tools")
-    println("6) Customer satisfaction by rapid, continous delivery of useful software")
-    println("7) Late Changes in requirement are welcomed")
-    println("8) Close, daily cooperation between business people and developers")
-    println("9) Predictable costs and Schedule")
-    println("10) Produce important merics like lead time, cycle time and throughput that  helps measures the teams performance, identify bottleneck")
+fun main() {
+    val list: List<Int> = listOf(3, 5, 9, 2)
+    print(reverseNum(list))
+}
 
-    Println("Disadvantages of Agile Development")
-    println("1) Greater demands on develops and clients")
-    println("2) Lack of necessary documentation")
-    println("3) No finite end")
-    println("4) Project easily falls off track")
-    println("5) More time and commitment")
-
-
+fun reverseNum(list: List<Int>): List<Int> {
+    val filo = arrayListOf<Int>()
+    for (i in list.indices) {
+        val nums = list[list.size - 1 - i]
+        filo.add(nums)
+    }
+    return filo
 }
